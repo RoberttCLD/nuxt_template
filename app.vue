0bb72895541fd865d5467a38f5096d507dce08c7
robertt@clearlakesdental.com <robertt@clearlakesdental.com>
2025-06-06 11:34:22 -0500

    init

--- app.vue
+++ app.vue
@@ -1,4 +1,9 @@
+<script setup lang="ts">
+// Write your javascript code here
+</script>
+
 <template>
+  <!-- Replace with your form/rendered content -->
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
